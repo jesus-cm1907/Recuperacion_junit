99d6d0a986a0b5bfcee6e2691ad363c7e94a0da7
Jesus-cm1907 <jesuka2000@gmail.com>
2020-04-20 03:36:26 +0200

    Esta es la quinta funcion

---
+++ FUNCION5.java
@@ -0,0 +1,41 @@
+
+/**
+ * 
+ * FUNCION QUE DADO DOS NUMEROS Y UN CARACTER HAGA LO SIGUIENTE
+ * 
+ * (+)SUMA=NUMERO1 + NUMERO2.
+ * 
+ * (-)RESTA=NUMERO1-NUMERO2.
+ * 
+ * (*)MULTIPLICACION NUMERO1 * NUMERO2.
+ * 
+ * 
+ * 
+ */
+public class FUNCION5 {
+	public FUNCION5() {
+	}
+
+	public int funcion5(String caracter, int numero1, int numero2) {
+
+		int valor = 0;
+
+		if (caracter == "+") {
+
+			valor = numero1 + numero2;
+
+		} else if (caracter == "-") {
+
+			valor = numero1 - numero2;
+
+		} else if (caracter == "*") {
+
+			valor = numero1 * numero2;
+
+		}
+
+		return valor;
+
+	}
+
+}

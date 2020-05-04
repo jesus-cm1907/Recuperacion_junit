cf4ab85a7a5b3ea9ebc5fd71949bce328cd2dcff
jesus-cm1907 <56446966+jesus-cm1907@users.noreply.github.com>
2020-05-05 01:04:35 +0200

    Update funcioness.java

--- funcioness.java
+++ funcioness.java
@@ -1,30 +1,77 @@
-package PruebasFinal;
+package Pruebas;
 
-public class funcioness {
+public class FUNCIONES {
 
-	public static int Entornos3(int[] numerosintroducidos) {
+	public FUNCIONES() {
 
-		int miEdad = 18;
-
-		int sumatorio = 0;
-
-		for (int i = 0; i < numerosintroducidos.length; i++) {
-
-			if (numerosintroducidos[i] >= (miEdad / 2) && numerosintroducidos[i] <= miEdad) {
-
-				sumatorio += numerosintroducidos[i];
-			}
-		}
-		return sumatorio;
 	}
 
-	public static String Entornos2(String palabra1, String palabra2) {
+	/**
+	 * 
+	 * FUNCION QUE AL INTRODUCIR UNA CADENA NOS DEVUELVA CUANTAS VOCALES CONTIENE.
+	 * 
+	 * 
+	 * 
+	 */
+    
+	
+	public boolean Entornos1(String palabra) {
+
+		int contador = 0;
+		int edad = 18;
+		boolean result = false;
+		
+		if(palabra == null) {
+			return false;
+		}
+
+		for (int i = 0; i < palabra.length(); i++) {
+			
+			//COMPARO CADA CARACTER CON LA VOCAL SIN TILDE
+			if ((palabra.toLowerCase().charAt(i) == 'a') || (palabra.toLowerCase().charAt(i) == 'e') || (palabra.toLowerCase().charAt(i) == 'i')
+					|| (palabra.toLowerCase().charAt(i) == 'o' || (palabra.toLowerCase().charAt(i) == 'u'))) {
+
+				contador++;
+			}
+			
+			//COMPARO CADA CARACTER CON LA VOCAL CON TILDE
+			if ((palabra.toLowerCase().charAt(i) == 'á') || (palabra.toLowerCase().charAt(i) == 'é') || (palabra.toLowerCase().charAt(i) == 'í')
+					|| (palabra.toLowerCase().charAt(i) == 'ó' || (palabra.toLowerCase().charAt(i) == 'ú'))) {
+
+				contador++;
+			}
+			
+		}
+		
+		if (contador >= edad) {
+			result = true;
+		}
+
+		return result;
+
+	}
+
+	/**
+	 * 
+	 * FUNCION QUE DADA DOS CADENAS NOS DEVUELVA CUAL TIENE MAS CONSONANTES
+	 * 
+	 */
+
+	public String Entornos2(String palabra1, String palabra2) {
 
 		int contadorConsonantes1 = 0;
 
 		int contadorConsonantes2 = 0;
 
-		String result = null;
+		String result = "No se reconocieron cadenas validas";
+		
+		if(palabra1 == null && palabra2 == null) {
+			return result;
+		}else if(palabra1 == null && palabra2 != null) {
+			return palabra2;
+		}else if(palabra1 != null & palabra2 == null) {
+			return palabra1;
+		}
 
 		contadorConsonantes1 = contarConsonantes(palabra1);
 
@@ -38,63 +85,99 @@ public class funcioness {
 
 			result = palabra2;
 
-		}
+		} else result = "Poseen la misma cantidad de consonantes";
 
 		return result;
 
 	}
 
-	private static int contarConsonantes(String cadena) {
+	private int contarConsonantes(String cadena) {
 
 		int contador = 0;
 
 		for (int i = 0; i < cadena.length(); i++) {
 
-			if ((cadena.charAt(i) != 'a') && (cadena.charAt(i) != 'e') && (cadena.charAt(i) != 'o')
-
-					&& (cadena.charAt(i) != 'i') && (cadena.charAt(i) != 'u')) {
-
-				contador++;
-			}
+			// CUENTA LA CANTIDAD DE CONSONANTES DE UNA CADENA
+			if((cadena.toLowerCase().charAt(i) == 'b')||
+					(cadena.toLowerCase().charAt(i) == 'c')||
+					(cadena.toLowerCase().charAt(i) == 'd')||
+					(cadena.toLowerCase().charAt(i) == 'f')||
+					(cadena.toLowerCase().charAt(i) == 'g')||
+					(cadena.toLowerCase().charAt(i) == 'h')||
+					(cadena.toLowerCase().charAt(i) == 'j')||
+					(cadena.toLowerCase().charAt(i) == 'k')||
+					(cadena.toLowerCase().charAt(i) == 'l')||
+					(cadena.toLowerCase().charAt(i) == 'm')||
+					(cadena.toLowerCase().charAt(i) == 'n')||
+					(cadena.toLowerCase().charAt(i) == 'ñ')||
+					(cadena.toLowerCase().charAt(i) == 'p')||
+					(cadena.toLowerCase().charAt(i) == 'q')||
+					(cadena.toLowerCase().charAt(i) == 'r')||
+					(cadena.toLowerCase().charAt(i) == 's')||
+					(cadena.toLowerCase().charAt(i) == 't')||
+					(cadena.toLowerCase().charAt(i) == 'v')||
+					(cadena.toLowerCase().charAt(i) == 'w')||
+					(cadena.toLowerCase().charAt(i) == 'x')||
+					(cadena.toLowerCase().charAt(i) == 'y')||
+					(cadena.toLowerCase().charAt(i) == 'z')){
+					    contador ++;
+					}
+			
 
 		}
 
 		return contador;
+
 	}
 
-	public static Boolean Entornos1(String palabra) {
+	/**
+	 * 
+	 * FUNCION QUE DADA UNA LISTA DE NUMEROS,DEVUELVA LA SUMA DE TODOS LOS QUE SEAN
+	 * 
+	 * * MENORES A TU EDAD PERO MAYORES QUE LA MITAD DE TU EDAD
+	 * 
+	 */
 
-		int contador = 0;
-		int edad = 18;
-		Boolean result = null;
+	public int Entornos3(int[] numerosintroducidos) {
 
-		for (int i = 0; i < palabra.length(); i++) {
+		int miEdad = 18;
 
-			if ((palabra.charAt(i) == 'a') || (palabra.charAt(i) == 'e') || (palabra.charAt(i) == 'i')
+		int sumatorio = 0;
+		if(numerosintroducidos == null) {
+			return sumatorio;
+		}
 
-					|| (palabra.charAt(i) == 'o' || (palabra.charAt(i) == 'u'))) {
+		for (int i = 0; i < numerosintroducidos.length; i++) {
 
-				contador++;
-				if (contador >= edad) {
-					result = true;
-				} else {
-					result = false;
+			if (numerosintroducidos[i] >= (miEdad / 2) && numerosintroducidos[i] <= miEdad) {
 
-				}
+				sumatorio += numerosintroducidos[i];
 
 			}
 
 		}
 
-		return result;
+		return sumatorio;
 
 	}
 
-	public static String Entornos4(int[] counter) {
+	/**
+	 * 
+	 * FUNCION QUE DADA UNA LISTA DE NOTAS ,DEVUELVA UNA CADENA INDICANDO SI LA
+	 * 
+	 * NOTA MEDIA HA SIDO INSUFICIENTE(<5),SUFICIENTE(>=5 y <6) BIEN (>=6 y
+	 * 
+	 * >7),NOTABLE(>=7 y <9) o SOBRESALIENTE(>=9 y 10)-
+	 * 
+	 * 
+	 * 
+	 */
 
-		int sum = 0;
+	public String Entornos4(int[] counter) {
 
-		int media;
+		float sum = 0;
+
+		double media;
 
 		String resultado = null;
 
@@ -120,7 +203,7 @@ public class funcioness {
 
 		} else if (media >= 7 && media < 9) {
 
-			resultado = "Notabale";
+			resultado = "Notable";
 
 		} else if (media >= 9 && media == 10) {
 
@@ -129,5 +212,5 @@ public class funcioness {
 		}
 
 		return resultado;
+
 	}
-}

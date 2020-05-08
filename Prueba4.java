12fb1eccff515fd6d35cee96a4a6721824425b62
jesus-cm1907 <56446966+jesus-cm1907@users.noreply.github.com>
2020-05-09 00:47:05 +0200

    Update Prueba4.java

--- Prueba4.java
+++ Prueba4.java
@@ -1,150 +1,339 @@
 package Pruebas;
 
+
+
 import org.junit.jupiter.api.Test;
+
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 import org.junit.jupiter.api.AfterAll;
+
+
 
 import org.junit.jupiter.api.AfterEach;
 
+
+
 import org.junit.jupiter.api.BeforeAll;
+
+
 
 import org.junit.jupiter.api.BeforeEach;
 
+
+
 import org.junit.jupiter.api.DisplayName;
 
-public class FUNCIONESTest3 {
+
+
+public class FUNCIONESTest4 {
+
+
 
 	static FUNCIONES funcioness = null;
+
+
+
 	static int ejecucionpruebas = 0;
+
+
 
 	@BeforeAll
 
+
+
 	static void prepararPruebas() {
+
+
 
 		System.out.println("Preparando pruebas....");
 
+
+
 		funcioness = new FUNCIONES();
 
+
+
 	}
+
+
 
 	@BeforeEach
 
+
+
 	private void EjecutarPruebas() {
+
+
 
 		ejecucionpruebas++;
 
+
+
 		System.out.println("CARGANDO EL TEST");
 
+
+
 	}
+
+
 
 	@AfterEach
 
+
+
 	private void terminadoPruebas() {
+
+
 
 		System.out.println("Se ha ejecutado el test numero...   " + ejecucionpruebas);
 
+
+
 	}
+
+
 
 	@AfterAll
 
+
+
 	static void finPruebas() {
+
+
 
 		System.out.println("Pruebas realizadas correctamente");
 
+
+
 		funcioness = null;
 
+
+
 	}
+
+
 
 	@Test
-	@DisplayName("TEST DE CAJA NEGRA CON VALOR DE LIMITE INFERIOR ")
 
-	void prueba1Funcion3() {
-		assertEquals(30, funcioness.Entornos3(new int[] { 9, 10, 11 }));
-		// PROBAMOS EL LIMITE INFERIOR EL CUAL SERIA 9 EN ESTE CASO Y ENTRARIA EN LA
-		// SUMA Y LE AÑADIMOS OTROS DOS VALORES PARA REALIZAR DICHA OPERACION.
-		//
+
+
+	@DisplayName("TEST DE CAJA DE PARTICIONES EQUIVALENTES(Entrada valida)")
+
+
+
+	void prueba1Funcion4() {
+
+
+
+		assertEquals("Suficiente", funcioness.Entornos4(new int[] { 6, 6, 3, 3, 3, 9 }));
+
+
+
+		// MEDIANTE ESTE TEST PROBAMOS QUE AL INTRODUCIR NUESTRAS NOTAS DE SEGUNDO
+
+
+
+		// TRIMESTRE LA MEDIA ES 5,5 POR LO TANTO TIENE QUE DEVOLVER SUFICIENTE
+
+
 
 	}
+
+
 
 	@Test
-	@DisplayName("TEST DE CAJA NEGRA CON VALOR DE LIMITE SUPERIOR")
 
-	void prueba2Funcion3() {
-		assertEquals(51, funcioness.Entornos3(new int[] { 18, 17, 16 }));
 
-		// PROBAMOS EL LIMITE SUPERIOR EL CUAL SERIA 18 EN ESTE CASO Y ENTRARIA EN LA
-		// SUMA ADEMAS LE AÑADIREMOS DOS VALORES PARA REALIZAR DICHA OPERACION.
+
+	@DisplayName("TEST DE CAJA NEGRA DE PARTICIONES EQUVALENTE(ENTRADA NO VALIDA) )")
+
+
+
+	void prueba2Funcion4() {
+
+
+
+		assertNotEquals("Notable", funcioness.Entornos4(new int[] { 6, 6, 3, 3, 3, 9 }));
+
+
+
+		// MEDIANTE ESTE TEST PROBAMOS QUE SI LE INTRODUCIMOS UNAS NOTAS DONDE NOS
+
+
+
+		// TENDRIA QUE DEVOLVER SUFICIENTE Y PONEMOS
+
+
+
+		// CON UN NOTEQUALS QUE NOS DEVUELVA NOTABLE LO RECONOCERA COMO ERROR.
+
+
+
 	}
+
+
 
 	@Test
-	@DisplayName(" TEST DE CAJA NEGRA LIMITE INFERIOR -1")
 
-	void prueba3Funcion3() {
-		assertEquals(9, funcioness.Entornos3(new int[] { 9, 8, 7 }));
-		// PROBAREMOS LOS VALORES QUE SE ENCUENTRAN POR DEBAJO DEL LIMETE INFERIOR(8,7)
-		// .SI REALIZAMOS LA PRUEBA VEREMOS QUE NO NOS SUMARA LOS INFERIORES AL LIMITE Y
-		// EL RESULTADO DE LA SU,A SERA SOLO 9.
-		//
+
+
+	@DisplayName("TEST DE CAJA NEGRA DE PARTICIONES EQUIVALENTE(ENTRADA VALIDA) ")
+
+
+
+	void prueba3Funcion4() {
+
+
+
+		assertEquals("Bien", funcioness.Entornos4(new int[] { 6, 6, 6, 6 }));
+
+
+
+		// MEDIANTE ESTE TEST PROBAMOS QUE NOS DEVUELVA TRAS INTRODUCIR CUATRO NUMEROS
+
+
+
+		// QUE LA MEDIA SEA BIEN
+
+
 
 	}
+
+
 
 	@Test
-	@DisplayName(" TEST DE CAJA NEGRA LIMITE SUPERIOR +1")
 
-	void prueba4Funcion3() {
-		assertEquals(18, funcioness.Entornos3(new int[] { 18, 19, 20 }));
-		// EN ESTE TEST HACEMOS JUSTO LO CONTRARIO, LO QUE HACEMOS ES QUE SOLO CUENTE EL
-		// NUMERO QUE SE ENCUENTRE EN EL LIMITE(18) MIENTRAS QUE LOS OTROS DOS NO LOS
-		// CONTARIA YA QUE
-		// SE ENCUENTRAN POR ENCIMA DEL LIMITE SUPERIOR
+
+
+	@DisplayName("TEST DE CAJA NEGRA DE PARTICIONES EQUIVALENTES(ENTRADA VALIDA)")
+
+
+
+	void prueba4Funcion4() {
+
+
+
+		assertEquals("Insuficiente", funcioness.Entornos4(new int[] { 0, 0, 0, 0 }));
+
+
+
+//CON ESTA FUNCION ESTAMOS COMPROBANDO DE QUE SI LE MANDAMOS QUE TODAS LOS NOTAS SON 0 ENTONCES LA MEDIA SERA INSUFICIENTE
+
+
 
 	}
+
+
 
 	@Test
-	@DisplayName("CAJA BLANCA COBERTURA DE DECISIONES ")
 
-	void prueba5Funcion3() {
-		assertEquals(41, funcioness.Entornos3(new int[] { 18, 11, 12 }));
-		// MEDIANTE ESTA DECISION QUEREMOS QUE MEDIANTE UNOS NUMEROS INTRODUCIDOS DENTRO
-		// DE LAS CONDICIONES IMPUESTAS NOS DEVUELVA LA SUMA DE DICHOS NUMEROS
+
+
+	@DisplayName("CAJA BLANCA COBERTURA DE CONDICIONES")
+
+
+
+	void prueba5Funcion4() {
+
+
+
+		assertEquals("Bien", funcioness.Entornos4(new int[] { 5, 5, 6, 8 }));
+
+
+
+//CON ESTE TEST COMPROBAMOS QUE SE CUMPLE LA CONDICION MEDIA<=6 && MEDIA<7,EN ESE CASO NOS DEVOLVERIA UN BIEN.
+
+
+
 	}
+
+
 
 	@Test
-	@DisplayName("CAJA BLANCA COBERTURA DE CONDICIONES ")
 
-	void prueba6Funcion3() {
-		assertEquals(30, funcioness.Entornos3(new int[] { 9, 10, 11, 20, 21, 21 }));
-		// MEDIANTE ESTE TEST ESTAMOS COMPRBANDO QUE SE CUMPLE LA CONDICION DE QUE SUME
-		// LOS NUMEROS MAYORES QUE 9 SIN EMBARGO LOS
-		// OTROS NUMEROS NO NOS LO SUMARA YA QUE NO CUMPLE LA OTRA CONDICION YA QUE SON
-		// MAYOR QUE 19.
+
+
+	@DisplayName("CAJA BLANCA COBERTURA DE CONDICIONES)")
+
+
+
+	void prueba6Funcion4() {
+
+
+
+		assertEquals("Insuficiente", funcioness.Entornos4(new int[] { 1, 9, 0, 7 }));
+
+
+
+		// CON ESTE TEST COMPROBAMOS QUE SE CUMPLE LA CONDICION MEDIA<5,EN ESTE CASO NOS
+
+
+
+		// DEVOLVERA INSUFICIENTE
+
+
 
 	}
+
+
 
 	@Test
-	@DisplayName("CAJA BLANCA COBERTURA DE CONDICIONES ")
 
-	void prueba7Funcion3() {
-		assertEquals(51, funcioness.Entornos3(new int[] { 17, 18, 16, 1, 3, 4 }));
-		// MEDIANTE ESTE TEST HACEMOS LO CONTRARIO,LO QUE QUEREMOS ES COMPROBAR LA
-		// SEGUNDA CONDICION EN LA QUE NOS SUME LOS NUMEROS MENORES QUE NUESTRA EDAD QUE
-		// ES 18
-		// MIENTRAS QUE LOS DEMAS NUMEROS NO NOS LO SUMARIA YA QUE NO CUMPLE LA
-		// CONDICION DE QUE SUME LOS MAYORES QUE LA MITAD DE 18.
+
+
+	@DisplayName("CAJA BLANCA COBERTURA DE DECISIONES)")
+
+
+
+	void prueba7Funcion4() {
+
+
+
+		assertEquals("Insuficiente", funcioness.Entornos4(new int[] { 3, 1, 8, 5 }));
+
+
+
+		// CON LA DECISION LLEVADA QUEREMOS QUE CON LAS NOTAS INTRODUCIDAS SEA DE MEDIA
+
+
+
+		// INSUFICIENTE
+
+
 
 	}
+
+
 
 	@Test
-	@DisplayName("CAJA BLANCA COBERTURA DE DECISIONES")
 
-	void prueba8Funcion3() {
-		assertEquals(0, funcioness.Entornos3(new int[] { 1, 2, 4, 21, 22, 23 }));
-		// MEDIANTE ESTA DECISIION COMPROBAMOS QUE SI INTRODUCIMOS VARIOS NUMEROS QUE NO
-		// CUMPLEN NINGUNA DE LAS DOS CONDICIONES ES DECIR QUE SEA MAYOR QUE 8 Y MENOR
-		// QUE 19 NOS
-		// DEVOLVERA 0
+
+
+	@DisplayName("CAJA BLANCA COBERTURA DE DECISIONES)")
+
+
+
+	void prueba8Funcion4() {
+
+
+
+		assertEquals("Notable", funcioness.Entornos4(new int[] { 7, 8, 6, 9 }));
+
+
+
+		// CON LA DECISION LLEVADA QUEREMOS QUE CON LAS NOTAS INTRODUCIDAS NOS DEVUELVA
+
+		// NOTABLE
+
+
+
 	}
+
+
 
 }

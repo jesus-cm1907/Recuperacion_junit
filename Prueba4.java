eaaa64fa60924792193a0832c52442f7694049e8
jesus-cm1907 <56446966+jesus-cm1907@users.noreply.github.com>
2020-05-09 00:52:36 +0200

    Update Prueba4.java

--- Prueba4.java
+++ Prueba4.java
@@ -106,12 +106,7 @@ public class FUNCIONESTest4 {
 
 		assertEquals("Suficiente", funcioness.Entornos4(new int[] { 6, 6, 3, 3, 3, 9 }));
 
-
-
 		// MEDIANTE ESTE TEST PROBAMOS QUE AL INTRODUCIR NUESTRAS NOTAS DE SEGUNDO
-
-
-
 		// TRIMESTRE LA MEDIA ES 5,5 POR LO TANTO TIENE QUE DEVOLVER SUFICIENTE
 
 
@@ -122,31 +117,15 @@ public class FUNCIONESTest4 {
 
 	@Test
 
-
-
 	@DisplayName("TEST DE CAJA NEGRA DE PARTICIONES EQUVALENTE(ENTRADA NO VALIDA) )")
-
-
 
 	void prueba2Funcion4() {
 
-
-
 		assertNotEquals("Notable", funcioness.Entornos4(new int[] { 6, 6, 3, 3, 3, 9 }));
 
-
-
 		// MEDIANTE ESTE TEST PROBAMOS QUE SI LE INTRODUCIMOS UNAS NOTAS DONDE NOS
-
-
-
 		// TENDRIA QUE DEVOLVER SUFICIENTE Y PONEMOS
-
-
-
 		// CON UN NOTEQUALS QUE NOS DEVUELVA NOTABLE LO RECONOCERA COMO ERROR.
-
-
 
 	}
 
@@ -154,49 +133,30 @@ public class FUNCIONESTest4 {
 
 	@Test
 
-
-
 	@DisplayName("TEST DE CAJA NEGRA DE PARTICIONES EQUIVALENTE(ENTRADA VALIDA) ")
-
 
 
 	void prueba3Funcion4() {
 
 
-
 		assertEquals("Bien", funcioness.Entornos4(new int[] { 6, 6, 6, 6 }));
 
-
-
 		// MEDIANTE ESTE TEST PROBAMOS QUE NOS DEVUELVA TRAS INTRODUCIR CUATRO NUMEROS
-
-
-
 		// QUE LA MEDIA SEA BIEN
 
-
-
-	}
+}
 
 
 
 	@Test
-
-
-
+	
 	@DisplayName("TEST DE CAJA NEGRA DE PARTICIONES EQUIVALENTES(ENTRADA VALIDA)")
-
-
 
 	void prueba4Funcion4() {
 
-
-
 		assertEquals("Insuficiente", funcioness.Entornos4(new int[] { 0, 0, 0, 0 }));
 
-
-
-//CON ESTA FUNCION ESTAMOS COMPROBANDO DE QUE SI LE MANDAMOS QUE TODAS LOS NOTAS SON 0 ENTONCES LA MEDIA SERA INSUFICIENTE
+	//CON ESTA FUNCION ESTAMOS COMPROBANDO DE QUE SI LE MANDAMOS QUE TODAS LOS NOTAS SON 0 ENTONCES LA MEDIA SERA INSUFICIENTE
 
 
 
@@ -205,22 +165,14 @@ public class FUNCIONESTest4 {
 
 
 	@Test
-
-
 
 	@DisplayName("CAJA BLANCA COBERTURA DE CONDICIONES")
 
-
-
 	void prueba5Funcion4() {
 
-
-
-		assertEquals("Bien", funcioness.Entornos4(new int[] { 5, 5, 6, 8 }));
-
-
-
-//CON ESTE TEST COMPROBAMOS QUE SE CUMPLE LA CONDICION MEDIA<=6 && MEDIA<7,EN ESE CASO NOS DEVOLVERIA UN BIEN.
+	assertEquals("Bien", funcioness.Entornos4(new int[] { 5, 5, 6, 8 }));
+	
+	//CON ESTE TEST COMPROBAMOS QUE SE CUMPLE LA CONDICION MEDIA<=6 && MEDIA<7,EN ESE CASO NOS DEVOLVERIA UN BIEN.
 
 
 
@@ -229,25 +181,14 @@ public class FUNCIONESTest4 {
 
 
 	@Test
-
-
-
+	
 	@DisplayName("CAJA BLANCA COBERTURA DE CONDICIONES)")
-
-
 
 	void prueba6Funcion4() {
 
-
-
 		assertEquals("Insuficiente", funcioness.Entornos4(new int[] { 1, 9, 0, 7 }));
 
-
-
 		// CON ESTE TEST COMPROBAMOS QUE SE CUMPLE LA CONDICION MEDIA<5,EN ESTE CASO NOS
-
-
-
 		// DEVOLVERA INSUFICIENTE
 
 
@@ -258,25 +199,14 @@ public class FUNCIONESTest4 {
 
 	@Test
 
-
-
 	@DisplayName("CAJA BLANCA COBERTURA DE DECISIONES)")
-
-
 
 	void prueba7Funcion4() {
 
-
-
 		assertEquals("Insuficiente", funcioness.Entornos4(new int[] { 3, 1, 8, 5 }));
 
-
-
-		// CON LA DECISION LLEVADA QUEREMOS QUE CON LAS NOTAS INTRODUCIDAS SEA DE MEDIA
-
-
-
-		// INSUFICIENTE
+	// CON LA DECISION LLEVADA QUEREMOS QUE CON LAS NOTAS INTRODUCIDAS SEA DE MEDIA
+	// INSUFICIENTE
 
 
 
@@ -286,25 +216,14 @@ public class FUNCIONESTest4 {
 
 	@Test
 
-
-
 	@DisplayName("CAJA BLANCA COBERTURA DE DECISIONES)")
-
-
 
 	void prueba8Funcion4() {
 
-
-
 		assertEquals("Notable", funcioness.Entornos4(new int[] { 7, 8, 6, 9 }));
 
-
-
 		// CON LA DECISION LLEVADA QUEREMOS QUE CON LAS NOTAS INTRODUCIDAS NOS DEVUELVA
-
 		// NOTABLE
-
-
 
 	}
 
